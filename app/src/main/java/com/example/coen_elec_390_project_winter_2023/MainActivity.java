e3ded1f98fac747598fa80b2aadba72b28ce6f76
BayanAlsalem <b_alsa@live.concordia.ca>
2023-02-26 09:43:12 -0500

    Github and android studio integration

---
+++ app/src/main/java/com/example/coen_elec_390_project_winter_2023/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.coen_elec_390_project_winter_2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
