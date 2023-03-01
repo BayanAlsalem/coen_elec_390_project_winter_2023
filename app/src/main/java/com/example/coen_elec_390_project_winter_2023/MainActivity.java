4d6f659219500e41efd9589ce6ee1407a1f545d8
BayanAlsalem <b_alsa@live.concordia.ca>
2023-02-28 20:41:56 -0500

    Main Activity Screen

--- app/src/main/java/com/example/coen_elec_390_project_winter_2023/MainActivity.java
+++ app/src/main/java/com/example/coen_elec_390_project_winter_2023/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.coen_elec_390_project_winter_2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button doctorBtn;
+    private Button patientBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize views
+        doctorBtn = findViewById(R.id.doctorBtnID);
+        patientBtn = findViewById(R.id.patientBtnID);
+
+        doctorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+        patientBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
+
+
 }
