c8503370f051cc853dea1affc4620ed72cca75a0
MazennW <mazen@almaram.com>
2023-03-25 16:04:23 -0400

    Merged with new master commits.

--- app/src/main/java/com/example/coen_elec_390_project_winter_2023/Login/LoginActivity.java
+++ app/src/main/java/com/example/coen_elec_390_project_winter_2023/Login/LoginActivity.java
@@ -50,7 +50,7 @@ public class LoginActivity extends AppCompatActivity {
                         startActivity(intent);
                     } else if (user.getUserType() == userOptions.userType.DOCTOR){
                         //To Do: we need to pass user object to either dashboards (eg: putExtra)
-                        startActivity(new Intent(LoginActivity.this, DoctorDashboard.class));
+                        startActivity(new Intent(LoginActivity.this, DoctorDashboardActivity.class));
                     }
                     finish();
                 }
