e3541dc040dd7fac13a031573c380d1d41a2d339
BayanAlsalem <b_alsa@live.concordia.ca>
2023-03-28 23:03:45 -0400

    Doctor and Patient profiles

--- app/src/main/java/com/example/coen_elec_390_project_winter_2023/Dashboard/DoctorProfileActivity.java
+++ app/src/main/java/com/example/coen_elec_390_project_winter_2023/Dashboard/DoctorProfileActivity.java
@@ -6,20 +6,38 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.coen_elec_390_project_winter_2023.Controller.FirebaseHelper;
+import com.example.coen_elec_390_project_winter_2023.Models.Doctor;
 import com.example.coen_elec_390_project_winter_2023.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class DoctorProfileActivity extends AppCompatActivity {
-    //TODO Doctor is only allowed to edit when click on EDIT and then SAVE to store in the database
     //TODO the name of the doctor is always NULL!!
 
+    FirebaseHelper firebaseHelper = new FirebaseHelper();
+    Doctor doc_obj = new Doctor();
     private TextView doctorFullName;
     private EditText fullNameEditText;
     private EditText hospitalNameEditText;
     private EditText emailEditText;
     private EditText passwordEditText;
     private Button editButton, saveButton;
+
+
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference usersRef = db.collection("users");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +53,49 @@ public class DoctorProfileActivity extends AppCompatActivity {
         editButton = findViewById(R.id.editBtnDoctorID);
         saveButton = findViewById(R.id.saveBtnDoctorID);
 
+        // Disable the EditText fields initially
+        fullNameEditText.setEnabled(false);
+        hospitalNameEditText.setEnabled(false);
+        emailEditText.setEnabled(false);
+        passwordEditText.setEnabled(false);
+
+
         // Get the doctor name from the intent
-        String doctorNameString = getIntent().getStringExtra("doctorName");
-        doctorFullName.setText("Name of doctor: " + doctorNameString);
+      //  String doctorNameString = getIntent().getStringExtra("name");
+       // doctorFullName.setText("Name of doctor: " + doctorNameString);
+
+        usersRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+         @Override
+         public void onComplete(@NonNull Task<QuerySnapshot> task) {
+             if (task.isSuccessful()) {
+                 for (QueryDocumentSnapshot document : task.getResult()) {
+                     Doctor doctor = document.toObject(Doctor.class);
+                     doctorFullName.setText(doctor.getName());
+                 }
+             } else {
+                 System.out.println("Error getting documents: " + task.getException());
+             }
+         }
+     });
 
         // Set an onClickListener on the Edit button
         editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Enable the EditText fields for editing
+                fullNameEditText.setEnabled(true);
+                hospitalNameEditText.setEnabled(true);
+                emailEditText.setEnabled(true);
+                passwordEditText.setEnabled(true);
+
+                // Show the Save button and hide the Edit button
+                saveButton.setVisibility(View.VISIBLE);
+                editButton.setVisibility(View.GONE);
+            }
+        });
+
+        // Set an onClickListener on the Save button
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Get the values from the EditText fields
@@ -49,10 +104,16 @@ public class DoctorProfileActivity extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
-                // Save these values to the firestore
+                // Disable the EditText fields after saving
+                fullNameEditText.setEnabled(false);
+                hospitalNameEditText.setEnabled(false);
+                emailEditText.setEnabled(false);
+                passwordEditText.setEnabled(false);
 
+                // Show the Edit button and hide the Save button
+                saveButton.setVisibility(View.GONE);
+                editButton.setVisibility(View.VISIBLE);
             }
         });
     } // end of onCreate() method
 } // end of DoctorProfileActivity {} class
-
--- app/src/main/java/com/example/coen_elec_390_project_winter_2023/Dashboard/PatientProfileActivity.java
+++ app/src/main/java/com/example/coen_elec_390_project_winter_2023/Dashboard/PatientProfileActivity.java
@@ -14,47 +14,83 @@ public class PatientProfileActivity extends AppCompatActivity {
 //TODO Patient is only allowed to edit when click on EDIT and then SAVE to store in the database
     //TODO the name of the patient is always NULL!!
 
-        private TextView patientFullName;
-        private EditText fullNameEditText;
-        private EditText patientAgeEditText;
-        private EditText emailEditText;
-        private EditText passwordEditText;
-        private EditText doctorNameEditText;
-        private Button editButton, saveButton;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.patient_profile_layout);
+    private TextView patientFullName;
+    private EditText fullNameEditText;
+    private EditText patientAgeEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText doctorNameEditText;
+    private Button editButton;
+    private Button saveButton;
 
-            // Find views by their respective IDs
-            patientFullName = findViewById(R.id.fullname_field);
-            fullNameEditText = findViewById(R.id.fullNameID);
-            patientAgeEditText = findViewById(R.id.patientAgeID);
-            emailEditText = findViewById(R.id.emailSignUpID);
-            passwordEditText = findViewById(R.id.passwordSignupID);
-            doctorNameEditText = findViewById(R.id.doctorNameID);
-            editButton = findViewById(R.id.editBtnPatientID);
-            saveButton = findViewById(R.id.saveBtnPatientID);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patient_profile_layout);
 
-            // Get the patient name from the intent
-            String patientNameString = getIntent().getStringExtra("patientName");
-            patientFullName.setText("Name of patient: " + patientNameString);
+        // Find views by their respective IDs
+        patientFullName = findViewById(R.id.fullname_field);
+        fullNameEditText = findViewById(R.id.fullNameID);
+        patientAgeEditText = findViewById(R.id.patientAgeID);
+        emailEditText = findViewById(R.id.emailSignUpID);
+        passwordEditText = findViewById(R.id.passwordSignupID);
+        doctorNameEditText = findViewById(R.id.doctorNameID);
+        editButton = findViewById(R.id.editBtnPatientID);
+        saveButton = findViewById(R.id.saveBtnPatientID);
 
-            // Set an onClickListener on the Edit button
-            editButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    // Get the values from the EditText fields
-                    String fullName = fullNameEditText.getText().toString();
-                    String age = patientAgeEditText.getText().toString();
-                    String email = emailEditText.getText().toString();
-                    String password = passwordEditText.getText().toString();
-                    String doctorName = doctorNameEditText.getText().toString();
+        // Get the patient name from the intent
+        String patientNameString = getIntent().getStringExtra("patientName");
+        patientFullName.setText("Name of patient: " + patientNameString);
 
-                    // Save these values to the firestore
+        // Disable the EditText fields by default
+        fullNameEditText.setEnabled(false);
+        patientAgeEditText.setEnabled(false);
+        emailEditText.setEnabled(false);
+        passwordEditText.setEnabled(false);
+        doctorNameEditText.setEnabled(false);
 
-                }
-            });
+        // Set an onClickListener on the Edit button
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Enable the EditText fields for editing
+                fullNameEditText.setEnabled(true);
+                patientAgeEditText.setEnabled(true);
+                emailEditText.setEnabled(true);
+                passwordEditText.setEnabled(true);
+                doctorNameEditText.setEnabled(true);
+
+                // Show the Save button and hide the Edit button
+                saveButton.setVisibility(View.VISIBLE);
+                editButton.setVisibility(View.GONE);
+            }
+        });
+
+        // Set an onClickListener on the Save button
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get the values from the EditText fields
+                String fullName = fullNameEditText.getText().toString();
+                String age = patientAgeEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String doctorName = doctorNameEditText.getText().toString();
+
+                // Save these values to the firestore
+
+                // Disable the EditText fields again
+                fullNameEditText.setEnabled(false);
+                patientAgeEditText.setEnabled(false);
+                emailEditText.setEnabled(false);
+                passwordEditText.setEnabled(false);
+                doctorNameEditText.setEnabled(false);
+
+                // Show the Edit button and hide the Save button
+                saveButton.setVisibility(View.GONE);
+                editButton.setVisibility(View.VISIBLE);
+            }
+        });
     } // end of onCreate() method
 } // end of PatientProfileActivity {} class
