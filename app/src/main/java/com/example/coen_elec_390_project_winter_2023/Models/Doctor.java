db7513732f7645b6f5db7830b7e73cac84b98718
Moemenwaleed <moemenwaleed@almaram.com>
2023-04-03 23:39:11 -0400

    Fixing Login/Logout for Users

--- app/src/main/java/com/example/coen_elec_390_project_winter_2023/Models/Doctor.java
+++ app/src/main/java/com/example/coen_elec_390_project_winter_2023/Models/Doctor.java
@@ -9,11 +9,11 @@ public class Doctor extends User {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public Doctor(String name,String hospital, String email, String password, String uid, List<String> patientsList) {
+    public Doctor(String name, String hospital, String email, String password, String uid, List<String> patientsList) {
         this.name = name;
         this.hospital = hospital;
-        this.age = email;
-        this.experience = password;
+        this.email = email;
+        this.password = password;
         this.uid = uid;
         this.type = userOptions.userType.DOCTOR;
         this.patientsList = patientsList;
